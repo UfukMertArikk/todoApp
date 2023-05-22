6be4a0d88767d15285895f80ce0dcc31ccff6657
UfukMertArikk <ufukmert1997@gmail.com>
2023-05-22 13:37:11 +0300

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,55 +1,80 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import Form from "react-bootstrap/Form";
 
 function App() {
-const [newItem , setNewItem] = useState("");
-const[items,setItems]=useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
 
-function addItem(){
-  if(!newItem){
-    alert("Enter an item..")
-    return;
+  function addItem() {
+    if (!newItem) {
+      alert("Enter an item..");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+    setItems((oldItems) => [...oldItems, item]);
+    setNewItem("");
+  }
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
   }
 
-const item = {
-  id: Math.floor(Math.random()*1000),
-  value: newItem
-}
-setItems(oldItems => [...oldItems,item])
-setNewItem("");
-}
-function deleteItem(id){
-  const newArray = items.filter(item => item.id !== id );
-  setItems(newArray);
-}
-
   return (
-    <div className="App">
-     <h1>Todo List App</h1>
-
-
-     <input
-     type="text"
-     placeholder="Add an item"
-     value={newItem}
-     onChange={e => setNewItem(e.target.value)}
-     />
-
-<button onClick={()=> addItem()}> Add </button>
-<ul>
-{items.map(item =>{
-return(
-  <li key={item.id}>{item.value}
-  <button onClick={()=>deleteItem(item.id)} >X</button>
-  </li>
-)
-
-})}
-
-</ul>
-      
-    </div>
+    <>
+      <div className="container">
+        <h1>Todo List App</h1>
+        <div className="row">
+          <Form>
+            <div className="col-9">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Bugün ne yapılacak?"
+                  onChange={(e) => setNewItem(e.target.value)}
+                  value={newItem}
+                  required
+                />
+              </Form.Group>
+            </div>
+            <div className="col-3">
+              <Button
+                variant="success"
+                className="btn-sm" 
+                onClick={() => addItem()}
+              >
+                <FaPlus />
+              </Button>
+            </div>
+            <div className="col-12">
+              <ul>
+                {items.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      {item.value}
+                      <Button
+                        variant="danger"
+                        className="btn-sm"
+                        onClick={() => deleteItem(item.id)}
+                      >
+                        <FaTrash />
+                      </Button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvckrIVGUYfsp7oHJgQ5VxUnC6-LmX_3s",
+  authDomain: "todoapp-f3992.firebaseapp.com",
+  projectId: "todoapp-f3992",
+  storageBucket: "todoapp-f3992.appspot.com",
+  messagingSenderId: "667708468475",
+  appId: "1:667708468475:web:81c2af1ef241c7c263545e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
